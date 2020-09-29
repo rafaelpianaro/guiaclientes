9f5cbd208f02958cc5d93b7a9381c4abf6f7efc1
Rafael Pianaro <rafaelpianaro@gmail.com>
2020-09-29 15:13:47 -0300

    trabalhando com eventos

--- src/components/Cliente.vue
+++ src/components/Cliente.vue
@@ -1,10 +1,11 @@
 <template>
-  <div id="cliente">
+  <div :class="{'cliente': !isPremium,'cliente-premium' : isPremium}">
     <h4>Nome: {{ cliente.nome }}</h4>
     <p>Número: {{ cliente.numero }}</p>
     <p>Email: {{ cliente.email }}</p>
     <p v-if='showIdade === true'>Idade: {{ cliente.idade }}</p>
     <p v-else>Usuário escondeu a idade.</p>
+    <button @click="mudarCor($event)">Mudar cor</button>
   </div>
 </template>
 
@@ -12,19 +13,33 @@
 export default {
     data() {
         return {
-            showIdade: true
+            isPremium: false
         }
     },
     props: {
-            nome: String,
-            cliente: Object
+            cliente: Object,
+            showIdade: Boolean
+    },
+    methods: {
+        mudarCor: function($event){
+            console.log($event)
+            this.isPremium = !this.isPremium
         }
+    }
 }
 </script>
 
 <style scoped>
-    #cliente{
+    .cliente{
         background-color: bisque;
+        max-width: 400px;
+        height: 200px;
+        padding: 10px;
+        margin-top: 10px;
+    }
+    .cliente-premium{
+        background-color: rgb(97, 97, 97);
+        color: bisque;
         max-width: 400px;
         height: 200px;
         padding: 10px;
